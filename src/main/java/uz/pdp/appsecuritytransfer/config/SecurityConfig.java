f691cdd8da42c4a7603ce74ab1b9762df6eb7912
saylaww <saylaww@gmail.com>
2021-04-12 14:39:52 +0500

    first commit

---
+++ src/main/java/uz/pdp/appsecuritytransfer/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package uz.pdp.appsecuritytransfer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import uz.pdp.appsecuritytransfer.security.JwtFilter;
+import uz.pdp.appsecuritytransfer.service.MyAuthService;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final MyAuthService myAuthService;
+
+    final JwtFilter jwtFilter;
+
+    public SecurityConfig(@Lazy MyAuthService myAuthService, JwtFilter jwtFilter) {
+        this.myAuthService = myAuthService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/api/login").permitAll()
+                .anyRequest().authenticated();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(myAuthService);
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
